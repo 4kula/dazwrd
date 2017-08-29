2d602599f81f7872ea4b84f1bf56d239514a3ac4
4kula <oce4n@protonmail.com>
2017-08-29 16:33:49 -0600

    added helper class's header file and implemented first print

--- dazwrd/src/templar_archives.cpp
+++ dazwrd/src/templar_archives.cpp
@@ -1,9 +1,6 @@
 using namespace BWAPI;
 
-// Houses all of the reuseable functions this bot will need
-class TemplarArchives {
-    // simple communication black box
-    public static void _p(string raw) {
-        Broodwar->sendText(raw);
-    }
+void TemplarArchives::_p(std::string raw)
+{
+    Broodwar->sendText(raw);
 }
---
+++ dazwrd/src/templar_archives.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+// Houses all of the reuseable functions this bot will need
+class TemplarArchives {
+    public:
+        // say something as the bot
+        void _p(string);
+}
